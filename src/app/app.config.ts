881221d93e739aad22921530cc7d3aeb408525f1
Jason Melendez <jason.melendez@gmail.com>
2017-07-12 16:51:18 -0700

    Add blank config file

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+/*
+ * Add Firebase authentication here and an identifying name for your chats (login and profiles could be a next step
+ * instead of hardcoding the name here).
+ *
+ * To get your Firebase credentials, click the "Add Firebase to your web app" link in the Firebase console.
+ *
+ */
+export const config = {
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  },
+
+  authorName: "YourName"
+};
